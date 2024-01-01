0cb2b222eb5469f55f49dafa911b1c5697d297b4
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-01-01 18:02:53 +1030

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Woodrow Martyr on 1/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
